e974bb1bf92f6a1b1b2b12f0dfbe2861ba9cb757
jmacura <jeremiaszmacura99@gmail.com>
2022-08-22 17:15:29 +0200

    added linter CI

---
+++ backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "eqeqeq": "off",
+        "curly": "error",
+        "no-var": "error",
+        "quotes": [0, "single"]
+    }
+}
